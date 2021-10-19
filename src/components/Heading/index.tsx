913c8e1a572f59e94befa75b2bf1d663e543f39c
Nathalia Cristina <nathaliacristina20@hotmail.com>
2021-10-19 20:43:01 -0300

    first commit

---
+++ src/components/Heading/index.tsx
@@ -0,0 +1,33 @@
+import * as S from './styles';
+
+export type LineColors = 'primary' | 'secondary';
+
+export type HeadingProps = {
+  children: React.ReactNode;
+  color?: 'white' | 'black';
+  lineLeft?: boolean;
+  lineBottom?: boolean;
+  lineColor?: LineColors;
+  size?: 'small' | 'medium';
+};
+
+const Heading = ({
+  children,
+  color = 'white',
+  lineColor = 'primary',
+  lineLeft = false,
+  lineBottom = false,
+  size = 'medium'
+}: HeadingProps) => (
+  <S.Wrapper
+    color={color}
+    lineLeft={lineLeft}
+    lineBottom={lineBottom}
+    size={size}
+    lineColor={lineColor}
+  >
+    {children}
+  </S.Wrapper>
+);
+
+export default Heading;
